c17d0edbb6f4476a10613be1b55dde7fe8804840
Edsonjsf <edsonjsf1990@gmail.com>
2022-07-25 13:49:34 -0400

    Tipos básicos

---
+++ src/practicas/tipado.ts
@@ -0,0 +1,16 @@
+/* Tipos de Datos Basicos */
+let nombre: string = "nombre";
+let numero: number = 0;
+let booleano: boolean = false;
+let nulo: null = null;
+let indefinido: undefined = undefined;
+let arreglo: [] = [];
+let cualquierTipo: any = "";
+
+/* Combinaciones */
+let stringArray: string[] = ["nombre1", "nombre2"];
+let stringArray2: Array<string> = ["nombre1", "nombre2"];
+
+/* Multi Tipado */
+let multiple: string | number = "nombre";
+multiple = 1;
